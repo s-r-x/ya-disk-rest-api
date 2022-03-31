7deb70ad2157fbf681f45a8386900807ed444037
s-r-x <strax1990@gmail.com>
2022-03-31 14:33:45 +0500

    chore: fix vite production bundling for nodejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
+  plugins: [
+    {
+      ...resolve({
+        preferBuiltins: true,
+        browser: true,
+      }),
+      enforce: 'pre',
+      apply: 'build',
+    },
+  ],
   build: {
     target: 'es2018',
     lib: {
       entry: path.resolve(__dirname, 'src', 'index.ts'),
       name: 'YaDisk',
       fileName: format => `lib.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['axios'],
     },
   },
 });
